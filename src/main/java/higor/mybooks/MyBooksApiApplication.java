bdef3da53649bd5d65b2a5bbff60162f8a4d84c4
higor-nunes <higorn@dbserver.com.br>
2020-12-01 00:22:39 +0100

    Moved Feign clients to domain module.

--- src/main/java/higor/mybooks/MyBooksApiApplication.java
+++ src/main/java/higor/mybooks/MyBooksApiApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
-  @EnableFeignClients
+@EnableFeignClients
 public class MyBooksApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/higor/mybooks/api/config/FeignClientConfiguration.java
+++ src/main/java/higor/mybooks/api/config/FeignClientConfiguration.java
@@ -1,22 +1,14 @@
 package higor.mybooks.api.config;
 
-import com.fasterxml.jackson.databind.Module;
 import feign.RequestInterceptor;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.hateoas.mediatype.hal.Jackson2HalModule;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 
 @Configuration
 public class FeignClientConfiguration {
-
-//  @Bean
-  public Module pageJacksonModule() {
-    return new Jackson2HalModule();
-//    return new PageJacksonModule();
-  }
 
   @Bean
   public RequestInterceptor requestInterceptor(OAuth2ProtectedResourceDetails details) {
